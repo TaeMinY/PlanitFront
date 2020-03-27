4dee52eb1bc84540a522dd24be1406f0836610db
UniTaeMin <onnu0802@naver.com>
2020-03-27 20:27:34 +0900

    File separation2

--- backend/src/Routers/index.ts
+++
@@ -1,8 +0,0 @@
-import * as express from "express"
-import Account from "./account/account.router"
-import Post from "./post/post.router"
-
-const router = express.Router();
-router.use("/account", Account)
-router.use("/post",Post)
-export default router
--- frontend/src/components/memo_plus.vue
+++
@@ -1,8 +0,0 @@
-<template>
-	<textarea class="text_input" placeholder="메모하기"></textarea>
-</template>
-<script>
-
-</script>
-<style scoped>
-</style>
